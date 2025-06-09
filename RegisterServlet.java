24a46c13e10bf638a308bb8542a15d5774706638
TITHEE05 <144910842+TITHEE05@users.noreply.github.com>
2025-06-10 00:06:58 +0530

    Create RegisterServlet.java

---
+++ RegisterServlet.java
@@ -0,0 +1,12 @@
+package com.college.complaints;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class RegisterServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Registration logic here
+    }
+}
