48ad839112f231c21aab469270a787aabfc9ff15
TITHEE05 <144910842+TITHEE05@users.noreply.github.com>
2025-06-10 00:06:26 +0530

    Create LoginServlet.java

---
+++ LoginServlet.java
@@ -0,0 +1,12 @@
+package com.college.complaints;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Login logic here
+    }
+}
