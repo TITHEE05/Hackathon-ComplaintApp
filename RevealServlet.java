60884c1c817a6c11cfcdfe745f1dd8f1cd36518b
TITHEE05 <144910842+TITHEE05@users.noreply.github.com>
2025-06-10 00:07:26 +0530

    Create RevealServlet.java

---
+++ RevealServlet.java
@@ -0,0 +1,12 @@
+package com.college.complaints;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class RevealServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Reveal student details with max votes
+    }
+}
